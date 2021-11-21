ac2b9d2384f092636028a71d5e9c74d53b5bd661
DiegoPenna <72095374@ucsm.edu.pe>
2021-11-21 04:37:50 -0500

    Se añadio los ejercicios

---
+++ GOOGLE2/GOOGLE2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GOOGLE2.MvcApplication" Language="C#" %>
---
+++ GOOGLE2/GOOGLE2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GOOGLE2.Startup))]
+namespace GOOGLE2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
